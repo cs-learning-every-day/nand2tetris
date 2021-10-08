378735d064415cd8e5241c4ccabab8b3cf5305af
XmchxCoder <1394466835@qq.com>
2021-10-08 10:38:54 +0800

    proj7: 实现Parser

--- projects/07/VMTranslator/parser/parser.go
+++ projects/07/VMTranslator/parser/parser.go
@@ -4,41 +4,100 @@ import (
 	"bufio"
 	"fmt"
 	"io/fs"
+	"log"
+	"strings"
 )
 
-type Command string
+type CommandType int8
+
+const (
+	Arithmetic CommandType = iota
+	Push
+	Pop
+	Label
+	GoTo
+	If
+	Function
+	Return
+	Call
+	Nil
+)
 
 func Parser(file fs.File) {
 	scanner := bufio.NewScanner(file)
 
 	scanner.Split(bufio.ScanLines)
 
-	for scanner.Scan() {
-		cmd := Command(scanner.Text())
+	for hasMoreCommands(scanner) {
+		cmd := scanner.Text()
 		if isValidCommand(cmd) {
-			fmt.Println(cmd)
+			advance(cmd)
 		}
 	}
 
 }
 
-func advance(command Command) {
-
+func hasMoreCommands(scanner *bufio.Scanner) bool {
+	return scanner.Scan()
 }
 
-func commandType(command Command) Command {
-	return ""
+func advance(command string) {
+
+	cmdType := commandType(command)
+	argOne := arg1(command, cmdType)
+	argTwo := arg2(command, cmdType)
+
+	switch cmdType {
+	case Arithmetic:
+		fmt.Println("Arithmetic: " + argOne + " " + argTwo)
+	case Pop:
+		fmt.Println("Pop: " + argOne + " " + argTwo)
+	case Push:
+		fmt.Println("Push: " + argOne + " " + argTwo)
+	}
 }
 
-func arg1() {
-
+func commandType(cmd string) CommandType {
+	if strings.Contains(cmd, "push") {
+		return Push
+	} else if strings.Contains(cmd, "pop") {
+		return Pop
+	} else if strings.Contains(cmd, "add") ||
+		strings.Contains(cmd, "sub") {
+		return Arithmetic
+	} else {
+		log.Panicln("command type error")
+		return -1
+	}
 }
 
-func arg2() {
+func arg1(cmd string, cmdType CommandType) string {
+	cmds := strings.Split(cmd, " ")
+	if cmdType == Arithmetic {
+		return cmds[0]
+	}
 
+	if cmdType == Return {
+		return ""
+	}
+
+	return cmds[1]
 }
 
-func isValidCommand(command Command) bool {
+func arg2(cmd string, cmdType CommandType) string {
+	cmds := strings.Split(cmd, " ")
+
+	if cmdType != Push &&
+		cmdType != Pop &&
+		cmdType != Function &&
+		cmdType != Call {
+		return ""
+	}
+
+	return cmds[2]
+}
+
+func isValidCommand(command string) bool {
 	if command == "" ||
 		(command[0] == '/' && command[1] == '/') {
 		return false
