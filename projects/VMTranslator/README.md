40a0384b18f5589e306a1698d67ad0136d7e2034
XmchxCoder <1394466835@qq.com>
2021-12-08 18:24:04 +0800

    project8: 更新虚拟机用法

---
+++ projects/VMTranslator/README.md
@@ -0,0 +1,12 @@
+### 虚拟机
+
+Run
+    
+``go run ./entry/main directory filename`` 将整个目录下的.vm生成filename.asm
+
+``go run ./entry/main directory`` 将整个目录下的每个tmp.vm生成tmp.asm
+
+``go run ./entry/main file`` 将file.vm生成file.asm
+
+
+将.vm文件生成.asm文件
--- projects/VMTranslator/entry/main.go
+++ projects/VMTranslator/entry/main.go
@@ -4,6 +4,7 @@ import (
 	"VMTranslator/parser"
 	"VMTranslator/utils"
 	"io/fs"
+	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
@@ -17,6 +18,22 @@ func checkPathValid(path string) {
 	}
 
 	defer file.Close()
+}
+
+func getAllVmFiles(dirPath string) []fs.FileInfo {
+	files, err := ioutil.ReadDir(dirPath)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	res := make([]fs.FileInfo, 0)
+	for _, file := range files {
+		if strings.HasSuffix(file.Name(), ".vm") {
+			res = append(res, file)
+		}
+	}
+	return res
 }
 
 // sourcePath肯定是.vm文件
@@ -48,29 +65,39 @@ func createAsmFile(sourcePath string) {
 
 }
 
-func visitDirectory(dirPath string) {
-	err := filepath.Walk(dirPath, func(path string, info fs.FileInfo, err error) error {
-		// 跳过不是vm文件类型的
-		suffixIdx := strings.LastIndex(path, ".vm")
-		if suffixIdx < 0 {
-			return nil
-		}
+func visitDirectory(dirPath, filename string) {
+	if filename == "" {
+		err := filepath.Walk(dirPath, func(path string, info fs.FileInfo, err error) error {
+			// 跳过不是vm文件类型的
+			suffixIdx := strings.LastIndex(path, ".vm")
+			if suffixIdx < 0 {
+				return nil
+			}
 
-		if !info.IsDir() {
-			createAsmFile(path)
+			if !info.IsDir() {
+				createAsmFile(path)
+			}
+			return nil
+		})
+
+		if err != nil {
+			log.Fatal(err)
 		}
-		return nil
-	})
-	if err != nil {
-		log.Fatal(err)
+	} else {
+		files := getAllVmFiles(dirPath)
+		if len(files) == 0 {
+			log.Fatal("该目录下，没有vm文件！")
+		}
+		// TODO: 整合所有vm文件
 	}
+
 }
 
-func run(path string) {
+func run(path, filename string) {
 	checkPathValid(path)
 
 	if utils.IsDirectory(path) {
-		visitDirectory(path)
+		visitDirectory(path, filename)
 	} else {
 		createAsmFile(path)
 	}
@@ -78,7 +105,14 @@ func run(path string) {
 
 func main() {
 	if len(os.Args) < 2 {
-		log.Fatalf("usage: %s <target file> or <target directory>\n", os.Args[0])
+		log.Fatal("用法详见README")
 	}
-	run(os.Args[1])
+
+	var targetFileName = ""
+
+	if len(os.Args) == 3 {
+		targetFileName = os.Args[2]
+	}
+
+	run(os.Args[1], targetFileName)
 }
